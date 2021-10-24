42f4e42c4ef71d72a84c19c9c48b22792d5771ab
tokaevaAA <galynka@ymail.com>
2021-10-25 00:08:15 +0300

    dz5PoolPotokov try2: now it is compiling

--- include/afina/concurrency/Executor.h
+++ include/afina/concurrency/Executor.h
@@ -16,6 +16,7 @@ namespace Concurrency {
  * # Thread pool
  */
 class Executor {
+public:
     enum class State {
         // Threadpool is fully operational, tasks could be added and get executed
         kRun,
@@ -29,19 +30,21 @@ class Executor {
     };
 
     Executor(std::string name, std::size_t max_queue_size, std::size_t low_watermark, std::size_t high_watermark, std::size_t idle_time):
-     _name(std::move(name), _max_queue_size(max_queue_size), _low_watermark(low_watermark), _high_watermark(high_watermark), _idle_time(idle_time) ){
-         std::unique_lock<std::mutex> mylock(this->mutex);
-         for (int i=0; i<_low_watermark; i=i+1){
-             threads.emplace_back(std::thread([this] {return perform(this);}));
-         }
+     _name(std::move(name)), _max_queue_size(max_queue_size), _low_watermark(low_watermark), _high_watermark(high_watermark), _idle_time(idle_time) {
+         std::unique_lock<std::mutex> mylock(_mutex);
+         _state=State::kRun;
+     }
 
-    }
-    ~Executor(){
-        std::unique_lock<std::mutex> mylock(this->mutex);
-        if (state != Executor::State::kStopped){
-            Stop(true);
+    
+    void Start(){
+        //printf("In start!\n");
+        std::unique_lock<std::mutex> mylock(_mutex);
+        for (int i=0; i<_low_watermark; i=i+1){
+            _threads.emplace_back(std::thread([this] {return perform(this);}));
         }
     }
+
+    ~Executor(){}
 
     /**
      * Signal thread pool to stop, it will stop accepting new jobs and close threads just after each become
@@ -49,16 +52,18 @@ class Executor {
      *
      * In case if await flag is true, call won't return until all background jobs are done and all threads are stopped
      */
-    void Stop(bool await = false){
-        std::unique_lock<std::mutex> mylock(this->mutex);
-        state=Executor::State::kStopping;
-        empty_condition.notify_all();
-        tasks.clear();
+    void Stop(bool await = true){
+        //printf("In stop!\n");
+        std::unique_lock<std::mutex> mylock(_mutex);
+        _state=Executor::State::kStopping;
+        _empty_condition.notify_all();
         if (await){
-            while(!threads.empty()){
-                _cv_no_more_threads.wait(mylock);
+            while(!_threads.empty()){
+                _cv_wants_to_stop.wait(mylock);
             }
         }
+        _state=Executor::State::kStopped;
+        //printf("tasks.size=%ld\n",_tasks.size());
         
     }
 
@@ -70,21 +75,24 @@ class Executor {
      * execution finished by itself
      */
     template <typename F, typename... Types> bool Execute(F &&func, Types... args) {
+        //printf("In execute!\n");
         // Prepare "task"
         auto exec = std::bind(std::forward<F>(func), std::forward<Types>(args)...);
 
-        std::unique_lock<std::mutex> lock(this->mutex);
-        if (state != State::kRun || tasks.size() == _max_queue_size) {
+        std::unique_lock<std::mutex> mylock(_mutex);
+        if (_state != State::kRun || _tasks.size() == _max_queue_size) {
+            //printf("In execute false!\n");
             return false;
         }
-
+        
         // Enqueue new task
-        std::unique_lock<std::mutex> mylock(this->mutex);
-        if ((tasks.size() > threads.size()) && (threads.size() < _high_watermark){
-            threads.emplace_back(std::thread([this] {return perform(this);}));
+        
+        if ((_tasks.size() > _threads.size()) && (_threads.size() < _high_watermark)){
+            _threads.emplace_back(std::thread([this] {return perform(this);}));
         }
-        tasks.push_back(exec);
-        empty_condition.notify_one();
+        //printf("In execute push task!\n");
+        _tasks.push_back(exec);
+        _empty_condition.notify_one();
         return true;
     }
 
@@ -98,74 +106,87 @@ private:
     /**
      * Main function that all pool threads are running. It polls internal task queue and execute tasks
      */
-    friend void perform(Executor *executor){
-        //myfunc
-        auto func_finish_thread=[executor]{
+    
+    class SpecialClass{
+    private:
+        Executor* executor;
+    public:
+        SpecialClass(Executor* myexecutor){executor=myexecutor;}
+        ~SpecialClass(){
             auto this_thread=std::this_thread::get_id();
-            for (auto it=executor->threads.begin(); it<executor->threads.end(); i=i+1){
+            for (auto it=executor->_threads.begin(); it<executor->_threads.end(); it=it+1){
                 if (it->get_id()==this_thread){
                     (*it).detach();
-                    executor->threads.erase(it);
-                    if (executor->threads.empty()){
-                        executor->state=Executor::State::kStopped;
-                        executor->_cv_no_more_threads.notify_all();
-                    }
+                    //printf("detach\n");
+                    executor->_threads.erase(it);
+                    executor->_cv_wants_to_stop.notify_all();
                     return;
                 }
             }
-        };
-        std::function<void()> task;
-        while (executor->state == Executor::State::kRun){
+        }
+
+    };
+    friend void perform(Executor *executor){
+        auto myobj=SpecialClass(executor);
+
+        bool flagisrunning=true;
+        while (flagisrunning){
+            std::function<void()> task;
             {
-               std::unique_lock<std::mutex> mylock(executor->mutex);
-               while (executor->tasks.empty()){
-                   executor->empty_condition.wait_for(mylock, std::chrono::milliseconds(executor->idle_time));
-                   if (executor->tasks.empty() && executor->threads.size() > executor->_low_watermark){
-                       func_finish_thread();
+               std::unique_lock<std::mutex> mylock(executor->_mutex);
+               while (executor->_tasks.empty()){
+                   executor->_empty_condition.wait_for(mylock, std::chrono::milliseconds(executor->_idle_time));
+                   //printf("woke up  %d, %d,  %ld!\n", std::this_thread::get_id(), executor->_tasks.empty(), executor->_tasks.size());
+                   if (executor->_tasks.empty() && executor->_threads.size() > executor->_low_watermark){
                        return;
                    } 
+                   if (executor->_tasks.empty() && executor->_state!=Executor::State::kRun){return;}
                } 
-               task=std::move(executor->tasks.front());
-               executor->tasks.pop_front();
+               task=std::move(executor->_tasks.front());
+               executor->_tasks.pop_front();
+               flagisrunning=executor->_state==Executor::State::kRun;
             }
+            
             try{task();}
             catch(const std::exception& myex){
-                std::cout<<myex.what()<<std::endl;
+                //printf("%s\n",myex.what());
+                std::terminate;
             }
         }
-        func_finish_thread();
+        std::unique_lock<std::mutex> mylock(executor->_mutex);
+        //and here work destructor for SpecialClass
     }
 
     /**
      * Mutex to protect state below from concurrent modification
      */
-    std::mutex mutex;
+    std::mutex _mutex;
 
     /**
      * Conditional variable to await new data in case of empty queue
      */
-    std::condition_variable empty_condition;
+    std::condition_variable _empty_condition;
 
     /**
      * Vector of actual threads that perorm execution
      */
-    std::vector<std::thread> threads;
+    std::vector<std::thread> _threads;
 
     /**
      * Task queue
      */
-    std::deque<std::function<void()>> tasks;
+    std::deque<std::function<void()>> _tasks;
 
     /**
      * Flag to stop bg threads
      */
-    State state;
+    State _state;
     std::string _name;
     std::size_t _low_watermark;
     std::size_t _high_watermark;
     std::size_t _max_queue_size;
     std::size_t _idle_time;
-    std::condition_variable _cv_no_more_threads;
+    std::condition_variable _cv_wants_to_stop;
 
 };
 
