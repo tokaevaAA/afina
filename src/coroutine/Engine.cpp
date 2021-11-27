5e874c4b6b696505aa3a258a830a2b6956f9bc10
tokaevaAA <galynka@ymail.com>
2021-11-27 20:35:45 +0300

    dz7CoroutineEngine try3: fixed allocation and with recursion

--- src/coroutine/Engine.cpp
+++ src/coroutine/Engine.cpp
@@ -3,6 +3,7 @@
 #include <setjmp.h>
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 
 
 namespace Afina {
@@ -10,19 +11,21 @@ namespace Coroutine {
 
 void Engine::Store(context &ctx) {
     volatile char a;
-    ctx.Low=(char*)(&a);
-    if (std::get<0>(ctx.Stack) != nullptr && (
-        std::get<1>(ctx.Stack) < ctx.High-ctx.Low  || std::get<1>(ctx.Stack) > 4*(ctx.High-ctx.Low ))){
-        delete[] std::get<0>(ctx.Stack);
-        ctx.Stack=std::make_tuple(new char[2*(ctx.High-ctx.Low)], 2*(ctx.High-ctx.Low));
+    ctx.Low = (char*)(&a);
+    auto &stack_ptr = std::get<0>(ctx.Stack);
+    auto &stack_size = std::get<1>(ctx.Stack);
+    auto required_size = ctx.High - ctx.Low;
+    if (stack_ptr == nullptr || stack_size < required_size || stack_size >= 2 * required_size) {
+        delete[] stack_ptr;
+        ctx.Stack=std::make_tuple(new char[required_size],required_size);
+        //stack_ptr = new char[required_size];
+        //stack_size = required_size;
     }
-    if (std::get<0>(ctx.Stack) == nullptr){
-        ctx.Stack=std::make_tuple(new char[2*(ctx.High-ctx.Low)], 2*(ctx.High-ctx.Low));
-    }
-    memcpy(std::get<0>(ctx.Stack),ctx.Low, ctx.High-ctx.Low);
+    memcpy(std::get<0>(ctx.Stack), ctx.Low, ctx.High - ctx.Low);
 }
 
 void Engine::Restore(context& ctx) {
+    //if we call Restore for idle_ctx, we want to enlarge stack not to hit smb's stack
     volatile char a;
     if (&ctx != idle_ctx && ctx.Low <= &a && &a <= ctx.High) {
         Restore(ctx);
@@ -38,9 +41,10 @@ void Engine::yield() {
     if (alive==nullptr){
         return;
     }
-    if (alive != cur_routine){ sched(alive);}
-    if (alive->next != nullptr) {sched(alive->next);}
-    else {return;}
+    if (alive != cur_routine){ sched(alive);} //we wont return from sched
+    else if (alive->next != nullptr) {sched(alive->next);}
+    //if here, then return 
+    
 }
 
 void Engine::sched(void *routine_) {
@@ -48,10 +52,14 @@ void Engine::sched(void *routine_) {
         return;
     }
     if (routine_==nullptr){
-        yield();
+        yield(); //go to yield,here cur_routine!=nullptr (because otherwise cur_routine=nullptr=routine_, hence we would have gone to 51 ) 
+                // if alive==nullptr, then return, no recursion
+                // if (alive != cur_routine){ sched(alive);} but alive !=nullptr, !=cur_routine then go to 62, no recursion
+                //if (alive == cur_routine), but alive->next != nullptr, alive->next != cur_routine, because alive==cur_rountine, then 62
+                //if (alive == cur_routine), but alive->next == nullptr, then return, no recursion
         return;
     }
-    if (cur_routine == nullptr){
+    if (cur_routine == nullptr){ //here we want to restore idle_ctx
         Restore(*(context*)(routine_));
         return;
     }
